5d449a36dfe108a8d7acd75ac6c1713ceceeebd2
Jennifer Chang <jenchang@iastate.edu>
2020-09-29 18:31:03 -0500

    pull paper titles and authors from xml

--- data/automagic.sh
+++ data/automagic.sh
@@ -7,6 +7,7 @@ set -u
 
 SEARCH_PMC=../bin/search_PMC.sh
 FETCH_PMC=../bin/fetch_PMC.sh
+XML_TSV=../bin/xml_to_tsv.pl
 
 # ==== Get PMC IDS
 while read TERM; do
@@ -28,3 +29,12 @@ while read TERM; do
     fi
 done < $1
 
+# ==== Get generate author paper lists
+while read TERM; do
+    OUTFILE=$(echo ${TERM} | tr '+' '_')
+    if [[ ! -f pmc/${OUTFILE}_papers.tsv ]]; then
+	sleep 1
+	echo "pulling papers and authors for $TERM... "
+	perl ${XML_TSV} "." pmc/${OUTFILE}_papers.tsv pmc/${OUTFILE}_authors.tsv pmc/${OUTFILE}.xml
+    fi
+done < $1
